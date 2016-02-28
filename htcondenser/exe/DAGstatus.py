3238ed1fae9ace71653f5ca762ba960122ff58a1
Robin Aggleton <robin.aggleton@cern.ch>
2016-02-28 10:53:51 +0000

    Refactor DAGstatus to split up a long function into smaller ones.
    Doesn not affect functionality at all.

--- htcondenser/exe/DAGstatus.py
+++ htcondenser/exe/DAGstatus.py
@@ -164,7 +164,7 @@ class StatusEnd(ClassAd):
         self.next_update = strip_doublequotes(next_update)
 
 
-def process(status_filename, summary):
+def process(status_filename, only_summary):
     """Main function to process the status file
 
     Parameters
@@ -172,7 +172,7 @@ def process(status_filename, summary):
     status_filename : str
         Name of status file to process.
 
-    summary : bool
+    only_summary : bool
         If True, only prints out summary of DAG. Otherwise prints out info about
         each job in DAG.
 
@@ -182,7 +182,24 @@ def process(status_filename, summary):
         If processing encounters block with unknown type
         (i.e. not DagStatus, NodeStatus or StatusEnd).
     """
+    dag_status, node_statuses, status_end = interpret_status_file(status_filename)
+    print_table(status_filename, dag_status, node_statuses, status_end, only_summary)
 
+
+def interpret_status_file(status_filename):
+    """Interpret the DAG status file, return objects with DAG & node statuses.
+
+    Parameters
+    ----------
+    status_filename : str
+        Filename of status file to interpret.
+
+    Returns
+    -------
+    DagStatus, list[NodeStatus], StatusEnd
+        Objects with info abotu DAG, all nodes, and end info (update times).
+
+    """
     dag_status = None
     node_statuses = []
     status_end = None
@@ -198,29 +215,12 @@ def process(status_filename, summary):
                 log.debug(contents)
                 # do something with contents here, depending on Type key
                 if contents['Type'].value == 'DagStatus':
-                    dag_status = DagStatus(timestamp=contents['Timestamp'].comment,
-                                           dag_status=contents['DagStatus'].comment,
-                                           nodes_total=contents['NodesTotal'].value,
-                                           nodes_done=contents['NodesDone'].value,
-                                           nodes_pre=contents['NodesPre'].value,
-                                           nodes_queued=contents['NodesQueued'].value,
-                                           nodes_post=contents['NodesPost'].value,
-                                           nodes_ready=contents['NodesReady'].value,
-                                           nodes_unready=contents['NodesUnready'].value,
-                                           nodes_failed=contents['NodesFailed'].value,
-                                           job_procs_held=contents['JobProcsHeld'].value,
-                                           job_procs_idle=contents['JobProcsIdle'].value)
+                    dag_status = generate_DagStatus(contents)
                 elif contents['Type'].value == 'NodeStatus':
-                    node = NodeStatus(node=contents['Node'].value,
-                                      node_status=contents['NodeStatus'].comment,
-                                      status_details=contents['StatusDetails'].value,
-                                      retry_count=contents['RetryCount'].value,
-                                      job_procs_queued=contents['JobProcsQueued'].value,
-                                      job_procs_held=contents['JobProcsHeld'].value)
+                    node = generate_NodeStatus(contents)
                     node_statuses.append(node)
                 elif contents['Type'].value == 'StatusEnd':
-                    status_end = StatusEnd(end_time=contents['EndTime'].comment,
-                                           next_update=contents['NextUpdate'].comment)
+                    status_end = generate_StatusEnd(contents)
                 else:
                     log.debug(contents)
                     log.debug(contents['Type'])
@@ -236,7 +236,8 @@ def process(status_filename, summary):
                 line_parsed = interpret_line(line)
                 contents[line_parsed.key] = line_parsed
     dag_status.node_statuses = node_statuses
-    print_table(status_filename, dag_status, node_statuses, status_end, summary)
+
+    return dag_status, node_statuses, status_end
 
 
 def interpret_line(line):
@@ -258,7 +259,39 @@ def interpret_line(line):
     return Line(key=parts[0], value=value, comment=comment)
 
 
-def print_table(status_filename, dag_status, node_statuses, status_end, summary):
+def generate_DagStatus(contents):
+    """Create, fill, and return a DagStatus object with info in contents dict."""
+    return DagStatus(timestamp=contents['Timestamp'].comment,
+                     dag_status=contents['DagStatus'].comment,
+                     nodes_total=contents['NodesTotal'].value,
+                     nodes_done=contents['NodesDone'].value,
+                     nodes_pre=contents['NodesPre'].value,
+                     nodes_queued=contents['NodesQueued'].value,
+                     nodes_post=contents['NodesPost'].value,
+                     nodes_ready=contents['NodesReady'].value,
+                     nodes_unready=contents['NodesUnready'].value,
+                     nodes_failed=contents['NodesFailed'].value,
+                     job_procs_held=contents['JobProcsHeld'].value,
+                     job_procs_idle=contents['JobProcsIdle'].value)
+
+
+def generate_NodeStatus(contents):
+    """Create, fill, and return a NodeStatus object with info in contents dict."""
+    return NodeStatus(node=contents['Node'].value,
+                      node_status=contents['NodeStatus'].comment,
+                      status_details=contents['StatusDetails'].value,
+                      retry_count=contents['RetryCount'].value,
+                      job_procs_queued=contents['JobProcsQueued'].value,
+                      job_procs_held=contents['JobProcsHeld'].value)
+
+
+def generate_StatusEnd(contents):
+    """Create, fill, and return a StatusEnd object with info in contents dict."""
+    return StatusEnd(end_time=contents['EndTime'].comment,
+                     next_update=contents['NextUpdate'].comment)
+
+
+def print_table(status_filename, dag_status, node_statuses, status_end, only_summary):
     """Print a pretty-ish table with important info
 
     Parameters
@@ -275,7 +308,7 @@ def print_table(status_filename, dag_status, node_statuses, status_end, summary)
     status_end : StatusEnd
         Object holding info about reporting.
 
-    summary : bool
+    only_summary : bool
         If True, only prints out summary of DAG. Otherwise prints out info about
         each job in DAG.
     """
@@ -315,7 +348,7 @@ def print_table(status_filename, dag_status, node_statuses, status_end, summary)
     summary_header = summary_format.format(*summary_dict.keys())
 
     # Now figure out how many char columns to occupy for the *** and ---
-    columns = len(summary_header) if summary else max(len(job_header), len(summary_header))
+    columns = len(summary_header) if only_summary else max(len(job_header), len(summary_header))
     columns += 1
     term_rows, term_columns = os.popen('stty size', 'r').read().split()
     term_rows = int(term_rows)
@@ -326,7 +359,7 @@ def print_table(status_filename, dag_status, node_statuses, status_end, summary)
     # Now actually print the table
     TColors.printc(status_filename, TColors.formatting_color('FILENAME'))
 
-    if not summary:
+    if not only_summary:
         # Print info for each job.
         print "~" * columns
         print job_header
@@ -341,7 +374,7 @@ def print_table(status_filename, dag_status, node_statuses, status_end, summary)
     print "-" * columns
     TColors.printc(summary_format.format(*[getattr(dag_status, v) for v in summary_dict.values()]),
                    TColors.status_color(dag_status.dag_status.split()[0]))
-    if not summary:
+    if not only_summary:
         # print time of next update
         print "-" * columns
         print "Status recorded at:", status_end.end_time
